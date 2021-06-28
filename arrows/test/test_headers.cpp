d143824fbfc6aa6dbdfca2a6027c41b250bdb685
Scott Burgert <scott.burgert@tamcaris.khq.kitware.com>
2021-06-28 18:27:05 -0400

    Added colmap test arrow

---
+++ arrows/test/test_headers.cpp
@@ -0,0 +1,14 @@
+#include <arrows/test/test_headers.h>
+
+namespace kwiver{
+    namespace arrows {
+        namespace test {
+            void test()
+            {
+                printf("Hello world");
+                // Initialize empty image as test
+                colmap::mvs::Image img();
+            }
+        }
+    }
+}
---
+++ arrows/test/test_headers.h
@@ -0,0 +1,17 @@
+#ifndef KWIVER_ARROWS_TEST_H
+#define KWIVER_ARROWS_TEST_H
+
+#include <arrows/test/kwiver_algo_test_export.h>
+#include <stdio.h>
+#include <colmap/mvs/image.h>
+
+namespace kwiver{
+    namespace arrows{
+        namespace test{
+            KWIVER_ALGO_TEST_EXPORT
+            void test();
+        }
+    }
+}
+#endif
+
